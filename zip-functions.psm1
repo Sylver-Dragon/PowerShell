b5dde1d2df9f15ffa1bc7641a04dae83f230f132
JohnLaska <johnlaska@yahoo.com>
2017-03-02 09:44:48 -0500

    zip-functions module

---
+++ zip-functions.psm1
@@ -0,0 +1,38 @@
+function New-Zip
+{
+	param([string]$zipfilename)
+	set-content $zipfilename ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+	(dir $zipfilename).IsReadOnly = $false
+}
+
+function Add-Zip
+{
+	param([string]$zipfilename)
+
+	if(-not (test-path($zipfilename)))
+	{
+		New-Zip $zipfilename	
+	}
+	
+	$shellApplication = new-object -com shell.application
+	$zipPackage = $shellApplication.NameSpace($zipfilename)
+	
+	foreach($file in $input) 
+	{ 
+            $zipPackage.CopyHere($file.FullName)
+            Start-sleep -milliseconds 500
+	}
+}
+
+function Extract-Zip
+{
+	param([string]$zipfilename, [string] $destination)
+
+	if(test-path($zipfilename))
+	{	
+		$shellApplication = new-object -com shell.application
+		$zipPackage = $shellApplication.NameSpace($zipfilename)
+		$destinationFolder = $shellApplication.NameSpace($destination)
+		$destinationFolder.CopyHere($zipPackage.Items())
+	}
+}
